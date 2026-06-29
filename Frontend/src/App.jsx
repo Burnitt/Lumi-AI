760366f4fb8c677a0791b82d76e7c4ae67dd560e
Gurnit Chopra <gurnitc@gmail.com>
2026-06-29 17:16:33 -0400

    Update App.jsx
    Sets up the routing between pages.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import CampaignCreator from './pages/CampaignCreator.jsx'
+import EmailComposer from './pages/EmailComposer.jsx'
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="campaign" element={<CampaignCreator />} />
+          <Route path="email" element={<EmailComposer />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
